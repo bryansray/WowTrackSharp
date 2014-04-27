9d29707e252edfd5b057d82bd3efe6a88972b785
Bryan Ray <bryan@bryanray.net>
2014-04-27 10:55:38 -0500

    Cleaning up the Autofac registration a bit.

--- LootTrack.Web/App_Start/AutofacConfig.cs
+++ LootTrack.Web/App_Start/AutofacConfig.cs
@@ -7,7 +7,6 @@ using Repository.Pattern.UnitOfWork;
 
 namespace LootTrack.Web
 {
-    using System.Data.Entity;
     using System.Reflection;
     using System.Web.Http;
     using System.Web.Mvc;
@@ -34,16 +33,10 @@ namespace LootTrack.Web
             builder.RegisterModule(new AutofacWebTypesModule());
             builder.RegisterSource(new ViewRegistrationSource());
 
-//            builder.RegisterType<DatabaseContext>().As<DatabaseContext>();
             builder.RegisterType<DatabaseContext>().As<IDataContextAsync>().WithParameter("nameOrConnectionString", "DefaultConnection").InstancePerHttpRequest();
             builder.RegisterType<UnitOfWork>().As<IUnitOfWorkAsync, IUnitOfWork>();
             builder.RegisterType<Entity>().As<IObjectState>();
-//            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerHttpRequest().InstancePerApiRequest();
             builder.RegisterGeneric(typeof(Repository<>)).As(typeof(IRepository<>)).InstancePerHttpRequest();
-
-//            builder.Register(context => context.Resolve<DbContextAdapter>()).As<IObjectSetFactory>().InstancePerHttpRequest();
-//            builder.Register(context => context.Resolve<DbContextAdapter>()).As<IObjectContext>().InstancePerHttpRequest();
-//            builder.Register(context => new DbContextAdapter(context.Resolve<DbContext>()));
 
             var container = builder.Build();
 
